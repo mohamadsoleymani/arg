4cbdbc76ddb8c18d00a04d10b86e357e390cf4f3
Mohammad Soleymani <mohammad.programmer2003@gmail.com>
2024-02-20 16:33:23 +0330

    edit components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,8 +8,8 @@ const Sidebar = () => {
     <div
       className={
         nav
-          ? "fixed z-10 right-0 top-0 w-[60%] h-full  bg-gray-100 ease-in-out duration-500"
-          : "ease-in-out duration-500 fixed right-[-100%]"
+          ? "fixed z-10 right-0 top-0 w-[60%] h-full backdrop-filter-xl bg-blue-400  ease-in-out duration-500"
+          : "ease-in-out duration-500  bg-white/30 fixed right-[-100%]"
       }
     >
       <button onClick={closeNav}>
--- src/pages/Moamelat.jsx
+++ src/pages/Moamelat.jsx
@@ -1,10 +1,24 @@
-import Chart from '../components/Chart'
-import Table2 from '../components/Table2';
+import Chart from "../components/Chart";
+import Table2 from "../components/Table2";
+import { useSelector } from "react-redux";
+
+import empty from "../assets/7486744.png";
+
 const Moamelat = () => {
+  const data = useSelector((state) => state.allDataChart.data);
   return (
     <div>
-      <Chart/>
-      <Table2 />
+      {data.length > 0 ? (
+        <div>
+          <Chart />
+          <Table2 />
+        </div>
+      ) : (
+        <div className="flex flex-col m-auto items-center">
+          <img src={empty} className="w-[250px] h-[250px] mt-16"></img>
+          <p className="text-sm font-bold mt-5 sm:text-lg">اطلاعات مورد نظر در این بازه زمانی وجود ندارد</p>
+        </div>
+      )}
     </div>
   );
 };
