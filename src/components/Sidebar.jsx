116123483e0881b65465be7cb9fc4fa2f85cb980
mohamadsoleymani <mmd.soleymaniasl@gmail.com>
2025-06-04 14:40:03 +0330

    fix sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
         className={classNames( 
           "fixed z-20 right-0 top-0 w-[70%] h-full bg-white ease-in-out duration-500 p-3 flex items-end flex-col",
           {"right-[-100%]": !nav}
-          ) 
+          )
     }
       >
         <button onClick={closeNav}>
